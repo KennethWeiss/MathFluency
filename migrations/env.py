c6b9130918f6cf1af60d132f700ee57fee993c0e
Kenny Weiss <kenny1weiss@aol.com>
2025-01-02 22:37:58 -0800

    chore: gunicorn

--- migrations/env.py
+++ migrations/env.py
@@ -1,6 +1,5 @@
 import logging
 from logging.config import fileConfig
-import os
 
 from flask import current_app
 
@@ -53,21 +52,33 @@ def get_metadata():
 
 
 def run_migrations_offline():
-    """Run migrations in 'offline' mode."""
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url, target_metadata=get_metadata(), literal_binds=True
     )
 
     with context.begin_transaction():
-        if os.environ.get('FLASK_MIGRATE_RESET'):
-            # Drop the alembic_version table
-            context.execute('DROP TABLE IF EXISTS alembic_version')
         context.run_migrations()
 
 
 def run_migrations_online():
-    """Run migrations in 'online' mode."""
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
 
     # this callback is used to prevent an auto-migration from being generated
     # when there are no changes to the schema
@@ -82,11 +93,6 @@ def run_migrations_online():
     connectable = get_engine()
 
     with connectable.connect() as connection:
-        if os.environ.get('FLASK_MIGRATE_RESET'):
-            # Drop the alembic_version table
-            connection.execute('DROP TABLE IF EXISTS alembic_version')
-            connection.commit()
-
         context.configure(
             connection=connection,
             target_metadata=get_metadata(),
