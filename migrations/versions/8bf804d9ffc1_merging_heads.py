03d7e21a0487aa2a6672546fee91fc415377c023
Kenny Weiss <kenny1weiss@aol.com>
2025-02-24 19:54:08 -0800

    add manual migration scripts to address missing revisions

---
+++ migrations/versions/8bf804d9ffc1_merging_heads.py
@@ -0,0 +1,24 @@
+"""Merging heads
+
+Revision ID: 8bf804d9ffc1
+Revises: 
+Create Date: 2025-02-24 19:53:49.878385
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8bf804d9ffc1'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
